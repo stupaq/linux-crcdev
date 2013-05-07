1ebc783566fc505f2b36f522410cd89d6f64bf06
Mateusz Machalica <mateuszmachalica@gmail.com>
2013-05-07 22:19:33 +0200

    Resolved some fisme questions in sysfs.

--- sysfs.c
+++ sysfs.c
@@ -10,7 +10,8 @@ static struct class *crc_sysfs_class = NULL;
 int __must_check crc_sysfs_init(void) {
 	int rv = 0;
 	crc_sysfs_class = class_create(THIS_MODULE, CRCDEV_CLASS_NAME);
-	if (IS_ERR_OR_NULL(crc_sysfs_class)) { // FIXME is null valid here?
+	/* Read the source, this can't be null is everything is OK */
+	if (IS_ERR_OR_NULL(crc_sysfs_class)) {
 		rv = ERROR(PTR_ERR(crc_sysfs_class));
 		crc_sysfs_class = NULL;
 	}
@@ -18,7 +19,7 @@ int __must_check crc_sysfs_init(void) {
 }
 
 void crc_sysfs_exit(void) {
-	if (crc_sysfs_class) { // FIXME is null valid here?
+	if (crc_sysfs_class) {
 		class_destroy(crc_sysfs_class);
 		crc_sysfs_class = NULL;
 	}
@@ -29,7 +30,8 @@ int __must_check crc_sysfs_add(struct pci_dev *pdev, struct crc_device *cdev) {
 	dev_t dev = crc_chrdev_getdev(cdev);
 	cdev->sysfs_dev = device_create(crc_sysfs_class, &pdev->dev, dev, cdev,
 			"crc%d", cdev->minor);
-	if (IS_ERR_OR_NULL(cdev->sysfs_dev)) { // FIXME is null valid here?
+	/* Read the source, this can't be null is everything is OK */
+	if (IS_ERR_OR_NULL(cdev->sysfs_dev)) {
 		rv = ERROR(PTR_ERR(cdev->sysfs_dev));
 		cdev->sysfs_dev = NULL;
 	}
@@ -37,7 +39,7 @@ int __must_check crc_sysfs_add(struct pci_dev *pdev, struct crc_device *cdev) {
 }
 
 void crc_sysfs_del(struct pci_dev *pdev, struct crc_device *cdev) {
-	if (cdev->sysfs_dev) { // FIXME is null valid here?
+	if (cdev->sysfs_dev) {
 		device_destroy(crc_sysfs_class, crc_chrdev_getdev(cdev));
 	}
 }
